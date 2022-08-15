08ba21443c801094021de76536211ebf9040b322
Farrukh Rasool <farrukhrasool112@gmail.com>
2022-08-15 23:17:03 +0500

    updated deployed path

--- client/src/components/PaymentForm.js
+++ client/src/components/PaymentForm.js
@@ -48,7 +48,7 @@ function PaymentForm() {
     if (!error && totalPrice !== 0) {
       try {
         const {id} = paymentMethod;
-        const response = await axios.post('https://test-ecommerce-eight.vercel.app/payment', {
+        const response = await axios.post('https://e-commerce-store-with-stripe-api.vercel.app/payment', {
           amount: totalPrice*100,
           id
         })
